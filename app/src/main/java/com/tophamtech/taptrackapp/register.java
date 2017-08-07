4d0353871ba3d1177f43ec86def6b7d343b29b08
James Topham <jimbotops@hotmail.co.uk>
2017-08-07 19:55:48 +0100

    Changed the initial activity to be simpler

---
+++ app/src/main/java/com/tophamtech/taptrackapp/register.java
@@ -0,0 +1,51 @@
+package com.tophamtech.taptrackapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class register extends AppCompatActivity {
+
+    //Setup view components
+    Button register;
+    EditText username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        register = (Button) findViewById(R.id.register);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked button by: "+username.getText()+password.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+
+}
