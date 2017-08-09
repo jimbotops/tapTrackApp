0df9409db5e3b863bdb447d6bb01ecbc193db69a
James Topham <jimbotops@hotmail.co.uk>
2017-08-09 22:20:14 +0100

    Added Get Method and callback to set JWT in register.java

--- app/src/main/java/com/tophamtech/taptrackapp/register.java
+++ app/src/main/java/com/tophamtech/taptrackapp/register.java
@@ -1,6 +1,5 @@
 package com.tophamtech.taptrackapp;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -45,13 +43,21 @@ public class register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Clicked button by: " + username.getText() + password.getText(), Toast.LENGTH_LONG).show();
-                popTextBox();
+                String userData[][] = {{"username", username.getText().toString()}, {"password", password.getText().toString()}};
+                rest.httpPost post = new rest().new httpPost();
+                post.execute(userData);
+                //helper.toastMaker(getApplicationContext(),post.execute(userData));
             }
         });
     }
 
-        public void popTextBox() {
-            helper.httpPost myPost = new helper().new httpPost();
-            myPost.execute();
+    public class sessionCreator {
+        String jwtMessage;
+
+        public void setJWT(String msg){
+            jwtMessage = msg;
+            Log.d("tester",jwtMessage);
         }
+    }
+
     }
